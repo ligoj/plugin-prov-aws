cbd36fcc70230dad8cc2cbeab1d164b506dc58f7
Fabrice Daugan <fabrice.daugan@gmail.com>
2022-08-17 01:52:12 +0200

    Improve CO2 matching for RDS instance types

--- src/main/java/org/ligoj/app/plugin/prov/aws/catalog/vm/rds/AwsPriceImportRds.java
+++ src/main/java/org/ligoj/app/plugin/prov/aws/catalog/vm/rds/AwsPriceImportRds.java
@@ -15,6 +15,7 @@ import org.ligoj.app.plugin.prov.aws.ProvAwsPluginResource;
 import org.ligoj.app.plugin.prov.aws.catalog.UpdateContext;
 import org.ligoj.app.plugin.prov.aws.catalog.vm.AbstractAwsPriceImportVm;
 import org.ligoj.app.plugin.prov.catalog.AbstractUpdateContext;
+import org.ligoj.app.plugin.prov.catalog.Co2Data;
 import org.ligoj.app.plugin.prov.dao.ProvQuoteDatabaseRepository;
 import org.ligoj.app.plugin.prov.model.AbstractCodedEntity;
 import org.ligoj.app.plugin.prov.model.ProvDatabasePrice;
@@ -59,6 +60,12 @@ public class AwsPriceImportRds extends
 	 * restriction.
 	 */
 	public static final String CONF_ETYPE = ProvAwsPluginResource.KEY + ":database-engine";
+
+	/**
+	 * <code>db.r5b.2xlarge</code> instance type like. See
+	 * https://aws.amazon.com/about-aws/whats-new/2021/09/amazon-rds-r5b-mysql-postgresql-databases/
+	 */
+	private static final Pattern RDS_B_INSTANCE_TYPE = Pattern.compile("db\\.[^.]+b\\..*$");
 
 	@Autowired
 	protected ProvQuoteDatabaseRepository qbRepository;
@@ -189,6 +196,15 @@ public class AwsPriceImportRds extends
 	}
 
 	@Override
+	protected Co2Data getCo2(final AbstractUpdateContext context, final String type) {
+		if (!context.getCo2DataSet().containsKey(type)) {
+			// Try to resolve the type without the "db." prefix
+			return super.getCo2(context, StringUtils.removeStart(type, "db."));
+		}
+		return super.getCo2(context, type);
+	}
+
+	@Override
 	protected LocalRdsContext newContext(final UpdateContext gContext, final ProvLocation region, final String term1,
 			final String term2) {
 		return new LocalRdsContext(gContext, iptRepository, dtRepository, dpRepository, qdRepository, region, term1,
@@ -196,12 +212,16 @@ public class AwsPriceImportRds extends
 	}
 
 	@Override
-	protected boolean isEnabledType(final AbstractUpdateContext context, final String type) {
-		return isEnabledDatabaseType(context, type);
+	protected CsvForBeanRds newReader(final BufferedReader reader) throws IOException {
+		return new CsvForBeanRds(reader);
 	}
 
 	@Override
-	protected CsvForBeanRds newReader(BufferedReader reader) throws IOException {
-		return new CsvForBeanRds(reader);
+	protected void copy(final LocalRdsContext context, final AwsRdsPrice csv, final ProvDatabaseType t) {
+		super.copy(context, csv, t);
+		if (RDS_B_INSTANCE_TYPE.matcher(t.getName()).matches()) {
+			// Overrid storage rate for r5b like type instances
+			t.setStorageRate(Rate.BEST);
+		}
 	}
 }
